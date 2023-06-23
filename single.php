ca3ab7335b76faaac75af1c46c538280877acd11
Kosuke.S <63331954+koromaru0717@users.noreply.github.com>
2023-06-24 08:08:23 +0900

    サイドバー作成 (#12)

--- single.php
+++ single.php
@@ -52,28 +52,7 @@
 
   </article>
 
-  <aside class="sidebar">
-    <div class="category_block">
-      <div class="block_title">
-        <div class="en_title">Ｃａｔｅｇｏｒｙ</div>
-        <div class="ja_title">カテゴリー</div>
-      </div>
-      <ul class="category_list">
-        <?php wp_list_categories('title_li=&depth=1'); ?>
-      </ul>
-    </div>
-
-    <div class="archive_block">
-      <div class="block_title">
-        <div class="en_title">Ａｒｃｈｉｖｅ</div>
-        <div class="ja_title">アーカイブ</div>
-      </div>
-      <ul class="archive_list">
-        <?php wp_get_archives(); ?>
-      </ul>
-    </div>
-
-  </aside>
+  <?php get_sidebar(); ?>
 </div>
 
 <?php get_footer(); ?>
