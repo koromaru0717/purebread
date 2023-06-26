062cea9a88be5640c6f8411c4ef8e1327c3bb049
kosuke <tusc.ssp.s.k@gmail.com>
2023-06-26 10:55:19 +0900

    #22 改修

--- functions.php
+++ functions.php
@@ -25,9 +25,6 @@ function theme_scripts()
 }
 add_action('wp_enqueue_scripts', 'theme_scripts');
 
-
-
-
 /**
  * タイトルタグの区切り文字をエン・ダッシュから縦線に変更する
  */
@@ -68,7 +65,8 @@ function the_title_max_length($length)
  * ブログ投稿タイプにカテゴリーとタグを追加
  * 
  */
-function add_categories_and_tags_to_cpt() {
+function add_categories_and_tags_to_cpt()
+{
   register_taxonomy_for_object_type('category', 'blogtop');
   register_taxonomy_for_object_type('post_tag', 'blogtop');
 }
@@ -78,7 +76,8 @@ add_action('init', 'add_categories_and_tags_to_cpt');
  * ブログトップページは最新記事＋15件を取得する
  * 
  */
-function modify_main_query($query) {
+function modify_main_query($query)
+{
   if (!is_admin() && $query->is_main_query()) {
     if (is_post_type_archive('blogtop')) {
       $query->set('posts_per_page', 16);
@@ -86,3 +85,30 @@ function modify_main_query($query) {
   }
 }
 add_action('pre_get_posts', 'modify_main_query');
+
+/**
+ * 日付アーカイブページのテンプレートを変更
+ */
+function custom_template_include($template)
+{
+  if (is_post_type_archive('blogtop') && is_date()) {
+    $custom_template = locate_template('date.php');
+    if (!empty($custom_template)) {
+      return $custom_template;
+    }
+  }
+  return $template;
+}
+add_filter('template_include', 'custom_template_include');
+
+/**
+ * カテゴリー検索を適応する
+ */
+function include_blogtop_in_category($query)
+{
+  if ($query->is_category() && $query->is_main_query()) {
+    $query->set('post_type', array('post', 'blogtop'));
+  }
+  return $query;
+}
+add_filter('pre_get_posts', 'include_blogtop_in_category');
