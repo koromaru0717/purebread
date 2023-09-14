f313447a01a3a6242f8fa64a94d4bf7b4f3c585a
Kou <63331954+koromaru0717@users.noreply.github.com>
2023-09-14 15:49:37 +0900

    ✨ ブログページ作成 (#65)

--- functions.php
+++ functions.php
@@ -3,4 +3,16 @@
 // アイキャッチを有効にする
 add_theme_support('post-thumbnails');
 
-?>
+
+/**
+ * 記事タイトルの最大文字数を制限
+ */
+function the_title_max_length($length)
+{
+  $title = get_the_title();
+  if (mb_strlen($title) > $length) {
+    $title = mb_substr($title, 0, $length);
+    $title = $title . '...';
+  }
+  echo $title;
+}
--- page.php
+++ page.php
@@ -1,19 +1,28 @@
 <?php get_header(); ?>
-<?php if (have_posts()) :
-  while (have_posts()) : the_post(); ?>
+<!-- セクションは共通のため、ヘッダー配下に移動 -->
+<section class="title-view">
+  <div class="page_text">
+    <h1><?php echo strtoupper($post->post_name); ?></h1>
+    <p><?php the_title(); ?></>
+  </div>
+</section>
 
-    <section class="title-view">
-      <div class="page_text">
-        <h1><?php echo strtoupper($post->post_name); ?></h1>
-        <p><?php the_title(); ?></>
-      </div>
-    </section>
-
-    <main id="main" class="wrapper">
-      <?php the_content(); ?>
-    </main>
-<?php endwhile;
-endif; ?>
+<main id="main" class="wrapper">
+<!-- スラッグを取得して、blogページかそれ以外かを分岐させる -->
+<?php
+  // 現在のページのスラッグを取得
+  global $post;
+  $post_slug = $post->post_name; ?>
+  <?php if ($post_slug === 'blog') {
+    get_template_part('blog-parts/blog', 'top');
+  } else {
+    if (have_posts()) :
+      while (have_posts()) : the_post();
+        the_content();
+      endwhile;
+    endif;
+  } ?>
+</main>
 
 
 <?php get_footer(); ?>
